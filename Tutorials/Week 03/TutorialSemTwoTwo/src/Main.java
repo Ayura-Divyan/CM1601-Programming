e23dba9a963c931dd84f964a0c74d88f02260c9e
Ayura-Divyan <ayuradivyan@gmail.com>
2026-02-04 12:23:54 +0530

    Question b complete

--- Tutorials/Week 03/TutorialSemTwoTwo/src/Main.java
+++ Tutorials/Week 03/TutorialSemTwoTwo/src/Main.java
@@ -2,9 +2,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new  Scanner(System.in);
         //Question a
         //There is an assumption that it is currently a common year
-        Scanner input = new  Scanner(System.in);
+        /*
         int leap_year_constant = 2;
 
         System.out.println("Enter the date:");
@@ -16,6 +17,18 @@ public class Main {
         int days = (((275 * month)/9) - (leap_year_constant * ((month + 9)/12)) + date - 31);
 
         System.out.println("You have to wait " + days + " days this year from 1st January for your birthday!");
-        input.close();
+        input.close();*/
+
+        //Quesiton b
+        /*
+        System.out.println("Enter your number:");
+        int number = input.nextInt();
+        number += 3;
+        number--;
+        System.out.println("Your number is: " + number);*/
+
+        //Question c
+
+
     }
 }
