39a18e29da39f2182cc06a74a7da36e425713048
Ayura-Divyan <ayuradivyan@gmail.com>
2026-02-09 09:30:21 +0530

    More Python stuff

--- Lectures/Week 04/Exercise 05.py
+++ Lectures/Week 04/Exercise 05.py
@@ -1,11 +1,19 @@
 num = 0
 try:
     num = int(input("Enter the number of novels: "))
-    if num > 20:
+    if (num > 20):
         print("Wow!")
-    elif num == 0:
+    elif (num == 0):
         print("Buy One Now!!")
     else:
         print("Not Bad")
 except ValueError:
     print("Error: Invalid data type, please enter only numbers")
+
+x = 7.0
+y = 3.5
+
+if (x/y) == 2.0:
+    print ("True")
+else:
+    print ("False")
