3a20d6e17099c4dbe2b14c0ac33f3d69380ff670
Ayura-Divyan <ayuradivyan@gmail.com>
2026-02-09 09:35:17 +0530

    Comapring Strings added

---
+++ Lectures/Week 04/Compaing Strings.py
@@ -0,0 +1,6 @@
+letter = ""
+letter = input("Enter a letter: ")
+letter = letter.upper()
+
+if (letter == "Y"):
+    print("Yes")
---
+++ Lectures/Week 04/Floats.py
@@ -0,0 +1,7 @@
+x = 7.0
+y = 3.5
+
+if (x/y) == 2.0:
+    print ("True")
+else:
+    print ("False")
