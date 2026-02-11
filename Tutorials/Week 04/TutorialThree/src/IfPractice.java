ee6a850718972005bb94b32e713b98e9df7014b3
Ayura-Divyan <ayuradivyan@gmail.com>
2026-02-11 14:25:36 +0530

    IfPractice Complete

---
+++ Tutorials/Week 04/TutorialThree/src/IfPractice.java
@@ -0,0 +1,16 @@
+public class IfPractice {
+    void main() {
+        int mark = 72;
+
+        if (mark >= 75) {
+            System.out.println("Grade A");
+        } else if (mark >=65) {
+            System.out.println("Grade B");
+        } else if (mark >=50){
+            System.out.println("Grade C");
+        } else {
+            System.out.println("Grade F");
+        }
+
+    }
+}
--- Tutorials/Week 04/TutorialThree/src/LateFeeCalculator.java
+++ Tutorials/Week 04/TutorialThree/src/LateFeeCalculator.java
@@ -18,3 +18,5 @@ public class LateFeeCalculator {
         input.close();
     }
 }
+
+
