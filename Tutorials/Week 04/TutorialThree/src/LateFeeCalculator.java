148f051ebca74821d07ccc4558146421c13412ee
Ayura-Divyan <ayuradivyan@gmail.com>
2026-02-11 14:21:02 +0530

    Late fee calculator complete

---
+++ Tutorials/Week 04/TutorialThree/src/LateFeeCalculator.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class LateFeeCalculator {
+    void main() {
+        Scanner input = new Scanner(System.in);
+        int daysLate;
+        System.out.println("Enter the number of days late:");
+        daysLate = input.nextInt();
+        double totalFee = 0.0;
+
+        if (daysLate > 0) {
+            totalFee += (daysLate * 20);
+        }
+
+        System.out.println("Days late: " + daysLate);
+        System.out.println("Total fee: " + totalFee);
+
+        input.close();
+    }
+}
