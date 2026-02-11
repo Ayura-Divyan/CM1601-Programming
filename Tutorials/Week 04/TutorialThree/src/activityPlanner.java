c8827f945176f55efdcbcfa40fdd7bdef4a54a8e
Ayura-Divyan <ayuradivyan@gmail.com>
2026-02-11 16:49:53 +0530

    Activity planner

---
+++ Tutorials/Week 04/TutorialThree/src/activityPlanner.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class activityPlanner {
+    void main() {
+        Scanner input = new Scanner(System.in);
+
+        int day;
+
+        System.out.println("Enter the day of the week number:");
+        day = input.nextInt();
+
+        switch (day) {
+            case 1:
+                System.out.println("Monday: Start-of-week review.");
+
+        }
+    }
+}
