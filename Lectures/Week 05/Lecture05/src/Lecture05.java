50fcdf871f1481cd7cd23b3115945483f69ce3ca
Ayura-Divyan <ayuradivyan@gmail.com>
2026-02-16 14:22:43 +0530

    For loop for even numbers

--- Lectures/Week 05/Lecture05/src/Lecture05.java
+++ Lectures/Week 05/Lecture05/src/Lecture05.java
@@ -1,6 +1,6 @@
 public class Lecture05 {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i+=2) {
+        for (int i = 0; i < 10; i += 2) {
             System.out.println(i);
         }
 
