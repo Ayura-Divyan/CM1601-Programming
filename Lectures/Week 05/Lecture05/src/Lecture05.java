7128f4f5ae249d0c53c03b6308b91f62d85f6e27
Ayura-Divyan <ayuradivyan@gmail.com>
2026-02-16 14:32:30 +0530

    While loop

--- Lectures/Week 05/Lecture05/src/Lecture05.java
+++ Lectures/Week 05/Lecture05/src/Lecture05.java
@@ -1,8 +1,17 @@
 public class Lecture05 {
-    public static void main(String[] args) {
+    static void main(String[] args) {
         for (int i = 0; i < 10; i += 2) {
             System.out.println(i);
         }
 
+        int w = 0;
+        while (w < 5) {
+            System.out.println(w);
+            w++;
+
+            if (w == 3) {
+                break;
+            }
+        }
     }
 }
