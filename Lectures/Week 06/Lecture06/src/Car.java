be3bfa29aa46a6bbc8c823ff7df572b491bbe58c
Ayura-Divyan <ayuradivyan@gmail.com>
2026-02-23 09:35:41 +0530

    Car class added with the field and method init

---
+++ Lectures/Week 06/Lecture06/src/Car.java
@@ -0,0 +1,17 @@
+public class Car {
+    //Fields
+    private String make;
+    private String model;
+    private int year;
+
+    public static void start() {
+
+    }
+    public static void stop() {
+
+    }
+    public static void drive(int speed) {
+
+    }
+}
+
