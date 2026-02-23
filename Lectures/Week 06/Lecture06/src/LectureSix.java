0584c759bc973861a6190ee0c9a4cd73bc44e198
Ayura-Divyan <ayuradivyan@gmail.com>
2026-02-23 09:23:16 +0530

    Changed the array to to string

--- Lectures/Week 06/Lecture06/src/LectureSix.java
+++ Lectures/Week 06/Lecture06/src/LectureSix.java
@@ -1,13 +1,12 @@
 import java.util.Arrays;
 
 public class LectureSix {
-    public static void modify(int[] target) {
-        target[0] = 100;
+    public static void modify(String[] target) {
+        target[0] = "100"; //This would take the parameter target which is an array from the main method and turn the first value to 100
     }
 
     public static void main(String[] args) {
-
-        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        String[] numbers = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
         LectureSix.modify(numbers);
         System.out.println(numbers[0]);
     }
