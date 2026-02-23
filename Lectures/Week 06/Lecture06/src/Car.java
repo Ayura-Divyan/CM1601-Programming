ccfc27efee2463cf6e036c7fdad730cf12b0a422
Ayura Divyan <ayuradivyan@gmail.com>
2026-02-23 09:46:45 +0530

    Update Car.java
    Operations added to methods

--- Lectures/Week 06/Lecture06/src/Car.java
+++ Lectures/Week 06/Lecture06/src/Car.java
@@ -1,17 +1,27 @@
 public class Car {
     //Fields
-    private String make;
-    private String model;
-    private int year;
+    private String make = "";
+    private String model = "";
+    private int year = 0;
 
-    public static void start() {
-
+    public String getMake(){
+        return this.make;
     }
-    public static void stop() {
 
+    public void setMake(String make) {
+        this.make = make;
     }
-    public static void drive(int speed) {
 
+    public void start() {
+        System.out.println("Started the " + this.model);
+    }
+
+    public void stop() {
+        System.out.println("Car stopped");
+    }
+    
+    public void drive(int speed) {
+        System.out.println("Car driving at " + speed);
     }
 }
 
