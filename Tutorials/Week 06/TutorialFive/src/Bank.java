7fae37ec8c1f89ea99bc5290a0c60f0b9e7a89a4
Ayura Divyan <ayuradivyan@gmail.com>
2026-02-25 09:36:18 +0530

    Two account objects created and added to bankAccount array list

--- Tutorials/Week 06/TutorialFive/src/Bank.java
+++ Tutorials/Week 06/TutorialFive/src/Bank.java
@@ -1,4 +1,13 @@
-public class Bank {
-    public static void main(String[] args) {
+import java.math.BigDecimal;
+import java.util.ArrayList;
 
+public class Bank {
+    private static ArrayList<Account> bankAccount = new ArrayList<>();
+    public static void main(String[] args) {
+        Account acc1 = new Account("33", "Max", new BigDecimal(1000000));
+        Account acc2 = new Account("44", "Moritz", new BigDecimal(36230564));
+
+        Bank.bankAccount.add(acc1);
+        Bank.bankAccount.add(acc2);
+    }
 }
