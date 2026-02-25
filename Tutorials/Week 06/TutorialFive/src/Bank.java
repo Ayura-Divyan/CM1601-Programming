ed5aea72ff9802b32005ed09c48340a043e1d7cf
Ayura Divyan <ayuradivyan@gmail.com>
2026-02-25 09:49:27 +0530

    Display method complete and called in the main method

--- Tutorials/Week 06/TutorialFive/src/Bank.java
+++ Tutorials/Week 06/TutorialFive/src/Bank.java
@@ -2,12 +2,23 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 
 public class Bank {
-    private static ArrayList<Account> bankAccount = new ArrayList<>();
+    private static ArrayList<Account> bankAccount = new ArrayList<Account>();
     public static void main(String[] args) {
         Account acc1 = new Account("33", "Max", new BigDecimal(1000000));
         Account acc2 = new Account("44", "Moritz", new BigDecimal(36230564));
 
         Bank.bankAccount.add(acc1);
         Bank.bankAccount.add(acc2);
+
+        //Display method created
+        Bank bank = new Bank();
+        bank.displayAccount(acc1);
+    }
+
+    //Display accounts method
+    public void displayAccount(Account account) {
+        System.out.print(account.getAccountNumber() + "     ");
+        System.out.print(account.getOwnerName() + "     ");
+        System.out.println(account.getBalance());
     }
 }
