43ce1de740a6ceb6d76b4995d659c6c23f7bef31
Ayura Divyan <ayuradivyan@gmail.com>
2026-02-25 08:53:56 +0530

    Classes and methods created

---
+++ Tutorials/Week 06/TutorialFive/src/Account.java
@@ -0,0 +1,43 @@
+public class Account {
+    //Fields
+    private String accountNumber;
+    private String ownerName;
+    private double balance;
+
+    //Methods
+    public Account(String accountNumber, String ownerName, double openingBalance) {
+
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public  void setOwnerName(String ownerName) {
+
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+
+    }
+
+    public void deposit(double amount) {
+
+    }
+
+    public Boolean withdraw(double amount) {
+        return withdraw;
+    }
+}
