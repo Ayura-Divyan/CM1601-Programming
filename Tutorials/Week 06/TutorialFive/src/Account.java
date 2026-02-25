bfb1e7578a2ce599a961fff520bdfb44d2db54c7
Ayura Divyan <ayuradivyan@gmail.com>
2026-02-25 09:23:46 +0530

    Setters, deposit and withdraw methods implemented

--- Tutorials/Week 06/TutorialFive/src/Account.java
+++ Tutorials/Week 06/TutorialFive/src/Account.java
@@ -2,44 +2,55 @@ import java.math.BigDecimal;
 
 public class Account {
     //Fields
-    private String accountNumber;
+    private  String accountNumber;
     private String ownerName;
     private BigDecimal balance;
 
     //Methods
     public Account(String accountNumber, String ownerName, BigDecimal openingBalance) {
-
+        this.accountNumber = accountNumber;
+        this.ownerName = ownerName;
+        this.balance = openingBalance;
     }
 
     public String getAccountNumber() {
-        return accountNumber;
+        return this.accountNumber;
     }
 
     public void setAccountNumber(String accountNumber) {
-
+        this.accountNumber = accountNumber;
     }
 
     public String getOwnerName() {
-        return ownerName;
+        return this.ownerName;
     }
 
     public  void setOwnerName(String ownerName) {
-
+        this.ownerName = ownerName;
     }
 
     public BigDecimal getBalance() {
-        return balance;
+        return this.balance;
     }
 
     public void setBalance(BigDecimal balance) {
-
+        this.balance = balance;
     }
 
     public void deposit(BigDecimal amount) {
-
+        if (amount.compareTo(BigDecimal.ZERO) > 0) {
+            this.balance = this.balance.add(amount);
+        } else {
+            System.out.println("Invalid deposit.");
+        }
     }
 
     public boolean withdraw(BigDecimal amount) {
-        return withdraw;
+        if (amount.compareTo(this.balance) > 0) {
+            return false; //Insufficient funds
+        } else {
+            this.balance = this.balance.subtract(amount);
+            return true; //Withdrawal successful
+        }
     }
 }
