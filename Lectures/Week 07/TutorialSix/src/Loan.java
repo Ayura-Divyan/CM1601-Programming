df1d59e83d1d99847cd121d9224d32398d87f24e
Ayura Divyan <ayuradivyan@gmail.com>
2026-03-04 08:55:24 +0530

    Constants added

---
+++ Lectures/Week 07/TutorialSix/src/Loan.java
@@ -0,0 +1,8 @@
+public class Loan {
+    public static void LoanType() {
+        final String PREMIUM_LOAN = "Premium Loan";
+        final String STANDARD_LOAN = "Standard Loan";
+        final String SMALL_LOAN = "Small Loan";
+    }
+
+}
