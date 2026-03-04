3082e87ac97d67062500be198eedb154d5573b46
Ayura Divyan <ayuradivyan@gmail.com>
2026-03-04 09:33:59 +0530

    Main method and loan logic added

--- Lectures/Week 07/TutorialSix/src/Loan.java
+++ Lectures/Week 07/TutorialSix/src/Loan.java
@@ -1,3 +1,15 @@
 public class Loan {
-
+    public String calculateLoanEligibility(double salary, int experience) {
+        if (salary >= 100000) {
+            if (experience >= 5) {
+                return ("Eligible for a " + LoanType.PREMIUM_LOAN);
+            } else {
+                return ("Eligible for a " + LoanType.STANDARD_LOAN);
+            }
+        } else if (50000 <= salary && salary <= 99999) {
+            return ("Eligible for a " + LoanType.SMALL_LOAN);
+        } else {
+            return "Not eligible for a loan";
+        }
+    }
 }
--- Lectures/Week 07/TutorialSix/src/LoanSystem.java
+++ Lectures/Week 07/TutorialSix/src/LoanSystem.java
@@ -1,3 +1,10 @@
 public class LoanSystem {
-
+     public static void main(String[] args) {
+         Loan loanObject = new Loan();
+         String loanDecision;
+         double salary = 100000;
+         int years = 5;
+         loanDecision = loanObject.calculateLoanEligibility(salary, years);
+         System.out.println(loanDecision);
+     }
 }
