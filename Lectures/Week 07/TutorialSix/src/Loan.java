bf033eae9d514d51879c411f58e2f532acd56ab2
Ayura Divyan <ayuradivyan@gmail.com>
2026-03-04 10:26:29 +0530

    Age eligibility loic added

--- Lectures/Week 07/TutorialSix/src/Loan.java
+++ Lectures/Week 07/TutorialSix/src/Loan.java
@@ -1,15 +1,34 @@
 public class Loan {
     public String calculateLoanEligibility(double salary, int experience) {
+        String eligibility = "Not eligible";
+
         if (salary >= 100000) {
             if (experience >= 5) {
-                return ("Eligible for a " + LoanType.PREMIUM_LOAN);
+                eligibility = LoanType.PREMIUM_LOAN;
             } else {
-                return ("Eligible for a " + LoanType.STANDARD_LOAN);
+                eligibility = LoanType.STANDARD_LOAN;
             }
-        } else if (50000 <= salary && salary <= 99999) {
-            return ("Eligible for a " + LoanType.SMALL_LOAN);
-        } else {
-            return "Not eligible for a loan";
         }
+        if (50000 <= salary && salary <= 99999) {
+            eligibility = LoanType.SMALL_LOAN;
+        }
+        return eligibility;
+    }
+
+    public String calculateLoanEligibility(double salary, int experience, int age) {
+            String loanType = calculateLoanEligibility(salary, experience);
+            String eligibility;
+
+            if (loanType.equals(LoanType.PREMIUM_LOAN)) {
+                if (age > 55) {
+                    eligibility = "Eligible for a premium loan - requires additional risk check";
+                } else {
+                    eligibility = "Eligible for high amount loan";
+                }
+            } else {
+                eligibility = loanType;
+            }
+
+            return eligibility;
     }
 }
--- Lectures/Week 07/TutorialSix/src/LoanSystem.java
+++ Lectures/Week 07/TutorialSix/src/LoanSystem.java
@@ -1,10 +1,23 @@
 public class LoanSystem {
      public static void main(String[] args) {
+
+         //tc 01
          Loan loanObject = new Loan();
          String loanDecision;
          double salary = 100000;
          int years = 5;
-         loanDecision = loanObject.calculateLoanEligibility(salary, years);
+         int age = 56;
+
+         loanDecision = loanObject.calculateLoanEligibility(salary, years, age);
+
+         System.out.println(loanDecision);
+
+         //tc 02
+         salary = 5000;
+          years = 2;
+          age = 56;
+
+         loanDecision = loanObject.calculateLoanEligibility(salary, years, age);
          System.out.println(loanDecision);
      }
 }
