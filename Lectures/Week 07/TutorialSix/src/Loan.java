ad827cde4ec48290e8ff0bd15184bf739d18a7de
Ayura Divyan <ayuradivyan@gmail.com>
2026-03-04 09:00:06 +0530

    LoanType class created to add the constants

--- Lectures/Week 07/TutorialSix/src/Loan.java
+++ Lectures/Week 07/TutorialSix/src/Loan.java
@@ -1,8 +1,3 @@
 public class Loan {
-    public static void LoanType() {
-        final String PREMIUM_LOAN = "Premium Loan";
-        final String STANDARD_LOAN = "Standard Loan";
-        final String SMALL_LOAN = "Small Loan";
-    }
 
 }
---
+++ Lectures/Week 07/TutorialSix/src/LoanType.java
@@ -0,0 +1,5 @@
+public class LoanType {
+    public static final String PREMIUM_LOAN = "Premium Loan";
+    public static final String STANDARD_LOAN = "Standard Loan";
+    public static final String SMALL_LOAN = "Small Loan";
+}
